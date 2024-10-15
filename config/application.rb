feaddd295a2b35c9c6b72968c5e34d7e5cc9133c
DESKTOP-7I6USA1\MITBo <githubbouya@gmail.com@yahoo.co.jp>
2024-10-15 20:53:07 +0900

    ログイン画面日本語化対応

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module BouyaTaskApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    
+    config.i18n.default_locale = :ja
   end
 end
