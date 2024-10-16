dc20873fc3eaad2ceb1a51a6aff0b83b723f1adf
DESKTOP-7I6USA1\MITBo <githubbouya@gmail.com@yahoo.co.jp>
2024-10-17 02:36:02 +0900

    board 一覧 (ダミー) 作成 board モデル作成

---
+++ app/models/board.rb
@@ -0,0 +1,10 @@
+# == Schema Information
+#
+# Table name: boards
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Board < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_15_070606) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_16_072756) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "boards", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.datetime "created_at", null: false
