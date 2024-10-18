efffc66699aed6e3b3def80ec57b1d5fca68c7b3
DESKTOP-7I6USA1\MITBo <githubbouya@gmail.com@yahoo.co.jp>
2024-10-19 00:56:38 +0900

    フォーム作成 (注意: user の値が入力されていませんとエラー表示がでるため更新はできない)

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -10,7 +10,26 @@ class BoardsController < ApplicationController
   end
 
   def new
-    # @board = Board.find(params[:id])
+    @board = Board.new
     # @board = current_user.boards.build
+  end
+
+  def create
+    # new で入力した form のデータを取得
+    @board = Board.new(board_params)
+    # DB に保存
+    if @board.save
+      redirect_to boards_path(@board), notice: '保存できたよ'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  # DB 保存する前にカラムに値が入っているかチェック
+  def board_params
+    params.require(:board).permit(:title, :description)
   end
 end
